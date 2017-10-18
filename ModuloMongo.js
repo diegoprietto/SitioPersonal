f137607b8b29fb4fe24e31ba717a29d1c8ad64e0
diegoprietto <dprsoftso@gmail.com>
2017-10-18 12:46:46 -0300

    Fix, cerrar los BD

--- ModuloMongo.js
+++ ModuloMongo.js
@@ -34,10 +34,12 @@ Qux.prototype.consultarDatos = function (callbackOk, callbackError, uri, colecci
 					if (callbackError) error(err);
 				}else{
 					//Retornar resultado de la consulta
-					if (callbackOk) callback(docs);
+					if (callbackOk) callbackOk(docs);
 				}
 			});
 		}
+		//Cerrar conexión
+		db.close();
 	});
 
 }
@@ -72,15 +74,15 @@ Qux.prototype.guardarArrayRegistros = function(callbackOk, callbackError, uri, c
 						//Ocurrió un error
 						if (callbackError) error(err);
 					}else{
-						//Éxito
-						db.close();
-
 						//Retornar resultado de la consulta
-						if (callbackOk) callback(result);
+						if (callbackOk) callbackOk(result);
 					}
 			});
 
 		}
+
+		//Cerrar conexión
+		db.close();
 	});
 }
 
@@ -115,14 +117,15 @@ Qux.prototype.guardarRegistro = function(callbackOk, callbackError, uri, colecci
 						if (callbackError) error(err);
 					}else{
 						//Éxito
-						db.close();
-
 						//Retornar resultado de la consulta
-						if (callbackOk) callback(result);
+						if (callbackOk) callbackOk(result);
 					}
 			});
 
 		}
+
+		//Cerrar conexión
+		db.close();
 	});
 }
 
